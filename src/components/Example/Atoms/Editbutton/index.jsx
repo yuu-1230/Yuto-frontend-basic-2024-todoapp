c9633175ef10468e82712c86cae3c9760e3b2a67
yuu-1230 <yuto.nagata1230@gmail.com>
2025-05-04 19:34:46 +0900

    editbuttonの追加

--- src/components/Example/Atoms/Editbutton/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import pencil from "../../../assets/svg/pencil.svg";
-
-export const Img = () => {
-  return <img src={pencil} />;
-};
-export const Editbutton = ({ onClick }) => {
-  return <button onClick={onClick}></button>;
-};
