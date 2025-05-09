a6d646f95b785924d6d7f98fbf0e9a98208fb50c
yuu-1230 <yuto.nagata1230@gmail.com>
2025-05-09 23:55:02 +0900

    mainからpull

---
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import COLOR from "../../../variables/color";
+import Plus from "../../../assets/svg/plus.svg";
+export const AddTaskButton = () => {
+  return (
+    <StyledAddTaskButton>
+      <StyledImg src={Plus} />
+      <StyledText>タスクを追加</StyledText>
+    </StyledAddTaskButton>
+  );
+};
+
+const StyledAddTaskButton = styled.div`
+  display: inline-flex;
+  justify-content: flex-start;
+  padding: 2px 6px;
+  gap: 10px;
+  font-size: 14px;
+  color: ${COLOR.GREEN};
+  border-radius: 12px;
+  &:hover {
+    background-color: ${COLOR.GREEN_TRANSLUCENT};
+  }
+`;
+const StyledImg = styled.img`
+  height: 20px;
+  width: 20px;
+`;
+const StyledText = styled.p`
+  margin: 0px;
+`;
