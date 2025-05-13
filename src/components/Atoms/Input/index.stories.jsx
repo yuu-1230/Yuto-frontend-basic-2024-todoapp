bc85f4ed84f74fa53278cd26ac9b9ef9fcbd600c
yuu-1230 <yuto.nagata1230@gmail.com>
2025-05-13 21:29:57 +0900

    inputfile作成

---
+++ src/components/Atoms/Input/index.stories.jsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+import React{useRef,useEffect} from "react";
+
+function Input = ({onEditComplete,defaultValue}) =>{
+    const inputref = useRef();
+    useEffect(()=>{
+
+    })
+}
