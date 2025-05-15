35956e0e5de15f3e4c6eec66dd65ce5d86185fa5
yuu-1230 <yuto.nagata1230@gmail.com>
2025-05-15 12:01:06 +0900

    未完成error一旦push

--- src/components/Molecules/Task/index.jsx
+++ src/components/Molecules/Task/index.jsx
@@ -1,1 +1,49 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Checkbox } from "../../Atoms/Checkbox";
+import { Editbutton } from "../../Atoms/Editbutton";
+import { Input } from "../../Atoms/Input";
 
+export const Task = ({
+  onTaskNameChange,
+  onTaskComplete,
+  taskName = "",
+  defaultIsEditing = false,
+}) => {
+  const isEditing = useState(defaultIsEditing);
+  const onEditComprete = 1;
+  return (
+    <StyledWrapper>
+      <StyledCheckboxWrapper></StyledCheckboxWrapper>
+      {isEditing ? (
+        Input(onEditComplete,taskName)
+      ) : (
+        <StyledNameAndButtonWrapper>
+           <StyledTaskName>
+            
+            </StyledTaskName>
+            <StyledEditButtonWrapper>
+            Editbutton()
+            <StyledEditButtonWrapper/>
+        <StyledNameAndButtonWrapper/>
+      )}
+    </StyledWrapper>
+  );
+};
+
+const StyledWrapper = styled.div`
+    
+`
+const StyledCheckboxWrapper = styled.div`
+    
+`
+const StyledNameAndButtonWrapper = styled.div`
+    
+`
+const StyledTaskName = styled.div`
+    
+`
+const StyledEditButtonWrapper = styled.div`
+    
+`
+ 
