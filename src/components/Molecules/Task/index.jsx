370bc5a336971d74066439989c7a2af3516e5a81
yuu-1230 <yuto.nagata1230@gmail.com>
2025-05-17 01:01:40 +0900

    ひな形作成

--- src/components/Molecules/Task/index.jsx
+++ src/components/Molecules/Task/index.jsx
@@ -1,2 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { Checkbox } from "../../Atoms/Checkbox";
+import { Editbutton } from "../../Atoms/Editbutton";
+import { Input } from "../../Atoms/Input";
+
+export const Task = () => {
+  return (
+    <StyledWrapper>
+      <StyledCheckboxWrapper>
+        <Checkbox />
+      </StyledCheckboxWrapper>
+      {isEditing ? (
+        <Input />
+      ) : (
+        <StyledNameAndButtonWrapper>
+          <StyledTaskName></StyledTaskName>
+          <StyledEditButtonWrapper>
+            <Editbutton />
+          </StyledEditButtonWrapper>
+        </StyledNameAndButtonWrapper>
+      )}
+    </StyledWrapper>
+  );
+};
+
+const StyledWrapper = styled.div``;
+
+const StyledCheckboxWrapper = styled.div``;
+
+const StyledNameAndButtonWrapper = styled.div``;
+
+const StyledTaskName = styled.div``;
+
+const StyledEditButtonWrapper = styled.div``;
