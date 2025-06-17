246c6d0bcb83aec088247c6362d513ef2e812a1a
yuu-1230 <yuto.nagata1230@gmail.com>
2025-06-17 22:38:45 +0900

    storage機能の追加

--- src/components/Organisms/TodoCard/index.jsx
+++ src/components/Organisms/TodoCard/index.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { AddTaskButton } from "../../Atoms/AddTaskButton";
 import { Task } from "../../Molecules/Task";
 import COLOR from "../../../variables/color";
 export const TodoCard = () => {
-  const [taskList, setTaskList] = useState([]);
+  const Storage_key = "taskitem";
+  const [taskList, setTaskList] = useState(() => {
+    const data = localStorage.getItem(Storage_key);
+    return data ? JSON.parse(data) : [];
+  });
   const onAddTaskButtonClick = () => {
     // setTaskList([...taskList, { name: "", initializing: true }]);
     const newitem = { name: "", initializing: true };
@@ -12,6 +16,9 @@ export const TodoCard = () => {
       return [...prev, newitem];
     });
   };
+  useEffect(() => {
+    localStorage.setItem(Storage_key, JSON.stringify(taskList));
+  }, [taskList]);
   const onTaskComplete = (index) => {
     setTaskList(taskList.filter((_, i) => i !== index));
   };
