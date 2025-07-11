0f1ec659e7760bc97169c9a1c11d6e27e5a39fc8
yuu-1230 <yuto.nagata1230@gmail.com>
2025-07-11 13:35:04 +0900

    index.jsxの修正

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { AlertHandlerProvider } from "./contexts/alert_handler";
 import MainPage from "./components/Pages/MainPage";
+import AlertManager from "./components/Organisms/AlertManager";
 import "./style.css";
 
-ReactDOM.render(<MainPage />, document.getElementById("app"));
+ReactDOM.render(
+  <AlertHandlerProvider>
+    <MainPage />
+    <AlertManager />
+  </AlertHandlerProvider>,
+  document.getElementById("app")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,54 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.jsx",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "docs"), // ← docsに出力
-    publicPath: "/Yuto-frontend-basic-2024-todoapp/", // ← GitHub Pages用のパス
-    clean: true, // 毎回クリア
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env", "@babel/preset-react"], // ←修正：react preset名
-            },
-          },
-        ],
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.svg$/,
-        use: ["url-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html", // ← srcでなくpublic
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    entry: "./src/index.jsx",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "docs"), // ← docsに出力
+      publicPath: isProduction ? "/Yuto-frontend-basic-2024-todoapp/" : "/", // ← 環境に応じて変更
+      clean: true, // 毎回クリア
     },
-    port: 8080,
-    open: true,
-  },
-  target: ["web", "es5"],
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                presets: ["@babel/preset-env", "@babel/preset-react"], // ←修正：react preset名
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.svg$/,
+          use: ["url-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html", // ← srcでなくpublic
+      }),
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      port: 8080,
+      open: true,
+    },
+    target: ["web", "es5"],
+  };
 };
